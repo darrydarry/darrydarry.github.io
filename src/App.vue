8e9bda850427471c05d2ce89e281ef323f36cf1e
DarryDarry <da.ershova@mail.ru>
2022-08-31 22:03:00 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <router-view />
+  </div>
+</template>
+
+<script>
+import "./assets/index.scss";
+
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
